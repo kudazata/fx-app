8b1d272f7f605845d1a472b5908232a414809b2c
Kuda <kudazata@gmail.com>
2023-04-29 10:01:41 +0200

    Initial Commit

---
+++ FX App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FX App
+//
+//  Created by Kuda Zata on 29/4/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
